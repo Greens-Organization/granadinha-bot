0979daa94197ed0b7bc2a10284e0d34c312ee4c2
AlphaLawless <alpha.vylly12@gmail.com>
2024-10-17 23:10:36 -0300

    feat: add collection custom

---
+++ src/infra/libs/collection/README.md
@@ -0,0 +1,5 @@
+## Project Collection
+
+## Reference
+
+- [@discordjs/collection](https://github.com/discordjs/discord.js/tree/main/packages/collection)
---
+++ src/infra/libs/collection/collection.ts
@@ -0,0 +1,55 @@
+/**
+ * @internal
+ */
+export interface CollectionConstructor {
+  new (): Collection<unknown, unknown>
+  new <Key, Value>(
+    entries?: readonly (readonly [Key, Value])[] | null
+  ): Collection<Key, Value>
+  new <Key, Value>(
+    iterable: Iterable<readonly [Key, Value]>
+  ): Collection<Key, Value>
+  readonly prototype: Collection<unknown, unknown>
+  readonly [Symbol.species]: CollectionConstructor
+}
+
+/**
+ * Separate interface for the constructor so that emitted js does not have a constructor that overwrites itself
+ * @internal
+ */
+export interface Collection<Key, Value> extends Map<Key, Value> {
+  constructor: CollectionConstructor
+}
+
+export class Collection<Key, Value> extends Map<Key, Value> {
+  /**
+   * Maps each item to another value into an array. Identical in behavior to
+   * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map | Array.map()}.
+   *
+   * @param fn - Function that produces an element of the new array, taking three arguments
+   * @param thisArg - Value to use as `this` when executing the function
+   * @example
+   * ```ts
+   * collection.map(user => user.tag);
+   * ```
+   */
+  public map<NewValue>(
+    fn: (value: Value, key: Key, collection: this) => NewValue
+  ): NewValue[]
+  public map<This, NewValue>(
+    fn: (this: This, value: Value, key: Key, collection: this) => NewValue,
+    thisArg: This
+  ): NewValue[]
+  public map<NewValue>(
+    fn: (value: Value, key: Key, collection: this) => NewValue,
+    thisArg?: unknown
+  ): NewValue[] {
+    if (typeof fn !== 'function') throw new TypeError(`${fn} is not a function`)
+    if (thisArg !== undefined) fn = fn.bind(thisArg)
+    const iter = this.entries()
+    return Array.from({ length: this.size }, (): NewValue => {
+      const [key, value] = iter.next().value
+      return fn(value, key, this)
+    })
+  }
+}
